3a5247e0577a7c7275752bc5bf3a1d1e3bb75495
Pranav Pai N <pranavpai0309@gmail.com>
2025-06-02 18:18:14 +0530

    Feat: Removed Extra Files

--- models/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import numpy as np
-
-with open("models/scheme_embeddings.pkl", "rb") as f:
-    embeddings = pickle.load(f)
-
-print(type(embeddings))        # should be <class 'numpy.ndarray'>
-print(embeddings.shape)        # e.g., (500, 384)
-print(embeddings[0])           # print the first embedding vector
